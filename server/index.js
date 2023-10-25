fb72c80a1f9c6e823aaa6603eb0603e299eda70b
Ricardo Miralles <ricmiber@gmail.com>
2023-10-25 20:17:21 +0200

    Adding client files and Socket.io library to server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,37 @@
 import express from 'express';
 import logger from 'morgan'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import {Server} from 'socket.io'
+import { createServer } from 'node:http';
 
-const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const port = process.env.PORT || 4000;
 
 const app = express();
+const server = createServer(app)
+const io =  new Server(server)
+io.on('connection',()=> {
+    console.log('User connected')
+})
 app.use(logger('dev'))
 
-app.get('/', (req, res) => {
-    res.send('<h1>Esto es el chat</h1>')
-})
+app.use(express.static(path.join(__dirname, '../client')))
 
-app.listen(port,()=>{
+// app.get('/', (req, res) => {
+//     res.sendFile(process.cwd() + '/client/style.css')
+//     res.sendFile(process.cwd() + '/client/index.html')
+// })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Send the HTML file
+});
+
+
+
+server.listen(port,()=>{
     console.log('listening on port '+port)
 })
