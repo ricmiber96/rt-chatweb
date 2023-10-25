0281bf0a3ae0a831e818a93434247c821cd4053f
Ricardo Miralles <ricmiber@gmail.com>
2023-10-25 19:28:21 +0200

    Initial commit 🚀

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import logger from 'morgan'
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    res.send('<h1>Esto es el chat</h1>')
+})
+
+app.listen(port,()=>{
+    console.log('listening on port '+port)
+})
